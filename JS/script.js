5070816430ba69c0e42214f01560f673ce3a699a
VICKY <vishujoshi21nov2000@gmail.com>
2022-10-24 10:08:00 +0530

    fixed set intervals

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,13 @@ let scoreH2 = document.getElementById('score');
 let timeLeftH2 = document.getElementById('timeLeft');
 let startNewGameButton = document.getElementById('startNewGame');
 let pauseGameButton = document.getElementById('pauseGame');
-
 let squares = document.querySelectorAll('.square');
+
+let score = 0;
+let timeLeft = 0;
+let hitPosition = null;
+let timerId = null;
+let randomMoleId = null;
 
 // Randomly Place Mole
 function randomeMole() {
@@ -11,21 +16,46 @@ function randomeMole() {
         square.classList.remove('mole');
     })
 
-    let hitPosition = Math.floor(Math.random() * squares.length);
-    squares[hitPosition].classList.add('mole');
+    let randomSquare = squares[Math.floor(Math.random() * squares.length)];
+    randomSquare.classList.add('mole');
+    hitPosition = randomSquare.id;
+}
+
+function countDown() {
+    --timeLeft;
+    timeLeftH2.innerHTML = `Time Left : ${timeLeft}`;
+
+    if (timeLeft === 0) {
+        // clearInterval(timerId);
+        // clearInterval(randomMoleId);
+    }
 }
 
 randomeMole();
 
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id === hitPosition) {
+            ++score;
+            scoreH2.innerHTML = `Your Score ${score}`;
+            hitPosition = null;
+        }
+    })
+})
+
+
 function startGame() {
     score = 0;
-    timeLeftH2 = 60;
+    timeLeft = 60;
 
     //call back  function
     //setInterval call function at regular interval
-    setInterval(randomeMole, 1000);
+    timerId = setInterval(randomeMole, 1000);
+    randomMoleId = setInterval(countDown, 1000);
 }
 
 
 startNewGameButton.addEventListener('click', startGame);
 startGame();
+
+pauseGameButton.add('click', pauseResumeGame);
